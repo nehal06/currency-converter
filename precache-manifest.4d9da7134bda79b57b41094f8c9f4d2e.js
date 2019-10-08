3e2c26c0c21508f957f1ebc391aaec2f6a789241
Nehal Jethwa <nehaljethwa6@gmail.com>
2019-10-08 21:59:38 +0530

    Updates

---
+++ precache-manifest.4d9da7134bda79b57b41094f8c9f4d2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee2bd0ef2317f5e799817b76b3dc2fd",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "d7f355d6aed82f8a4f99",
+    "url": "/currency-converter/static/css/main.c6760ff3.chunk.css"
+  },
+  {
+    "revision": "44ce4e3e22df4fb308bd",
+    "url": "/currency-converter/static/js/2.7dfa6a2b.chunk.js"
+  },
+  {
+    "revision": "d7f355d6aed82f8a4f99",
+    "url": "/currency-converter/static/js/main.ee6d7a80.chunk.js"
+  },
+  {
+    "revision": "9dc141e9cf9965a1e3f1",
+    "url": "/currency-converter/static/js/runtime-main.6e0d609b.js"
+  },
+  {
+    "revision": "1911e7786d46c7f6156e8398ee2125fb",
+    "url": "/currency-converter/static/media/Rose-Petals.1911e778.svg"
+  },
+  {
+    "revision": "9b72f45363c6296a144f84793107fa48",
+    "url": "/currency-converter/static/media/dollar-symbol.9b72f453.svg"
+  },
+  {
+    "revision": "49cf8bf3c1202b2992851089d926d803",
+    "url": "/currency-converter/static/media/rupee-indian.49cf8bf3.svg"
+  },
+  {
+    "revision": "23095266044c94c496416c5e8d26492f",
+    "url": "/currency-converter/static/media/usa.23095266.png"
+  }
+]);
